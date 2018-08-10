34eda96e9473c8a3f6d7ef383f8d63e8dbcff5a7
tarick ali <tarickaliabdi@gmail.com>
2018-08-10 11:42:02 +0300

    changed the logical operators

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
- function triangle(){
+function triangle(){
  var triangle;
  triangle = document.getElementById("enter a number").value;
  }
-var a= prompt("Enter the first number");
-var b= prompt("Enter the second number");
-var c= prompt("Enter the third number");
-if(a===b && b===c){
+var a= parseInt(prompt("Enter the first number"));
+var b= parseInt(prompt("Enter the second number"));
+var c= parseInt(prompt("Enter the third number"));
+
+if (a===""||b===""||c===""){
+alert("Try Again")
+}
+
+else if(a===b && b===c){
 alert( "This is an Equilateral triangle")
 }
 else if(a===b || b===c){
 alert("This is an Isosceles triangle")
 }
-else if(a!==b && b!==c){
-alert("This is a scalene triangle ")
-}
-else if(a+b<c || a+c<b || b+c<a){
+else if(a+b<c || a+c<b ||b+c<a){
 alert("NOT a triangle")
+}
+else if(a+b>c || a+c>b || b+c>a){
+alert("This is a scalene triangle")
+}
+else {
+alert("Try Again! Empty field")
+
 }
