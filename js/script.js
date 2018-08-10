10587bd751edb77df3ae8241db9292a663917e7d
tarick ali <tarickaliabdi@gmail.com>
2018-08-10 15:13:00 +0300

    added a photo

--- js/script.js
+++ js/script.js
@@ -1,54 +1,8 @@
-// function triangle(){
-//  var triangle;
-//  triangle = document.getElementById("enter a number").value;
-//  }
-// var a= parseInt(prompt("Enter the first number"));
-// var b= parseInt(prompt("Enter the second number"));
-// var c= parseInt(prompt("Enter the third number"));
-//
-// if (a===""||b===""||c===""){
-// alert("Try Again")
-// }
-//
-// else if(a===b && b===c){
-// alert( "This is an Equilateral triangle")
-// }
-// else if(a===b || b===c){
-// alert("This is an Isosceles triangle")
-// }
-// else if(a+b<c || a+c<b ||b+c<a){
-// alert("NOT a triangle")
-// }
-// else if(a+b>c || a+c<b || b+c>a){
-// alert("This is a scalene triangle")
-// }
-// else {
-// alert("Try Again")
-// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- function triangle(){
- var triangle;
- triangle = document.getElementById("enter a number").value;
- }
+function triangle(){
+var triangle;
+triangle = document.getElementById("enter a number").value;
+}
 var a= parseInt(prompt("Enter the first number"));
 var b= parseInt(prompt("Enter the second number"));
 var c= parseInt(prompt("Enter the third number"));
@@ -76,7 +30,7 @@ else {
 alert("Try Again")
 
 }
-var enters = ["Refresh","the page!"];
-enters.forEach(function(enter){
+var words = ["Refresh","the page!"];
+words.forEach(function(word){
 alert(word)
 }
