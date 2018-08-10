7d31166e7b1ad4f354d06e3e81f691c35a8c6ece
tarick ali <tarickaliabdi@gmail.com>
2018-08-10 12:36:27 +0300

    changed the array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,51 @@
-function triangle(){
+// function triangle(){
+//  var triangle;
+//  triangle = document.getElementById("enter a number").value;
+//  }
+// var a= parseInt(prompt("Enter the first number"));
+// var b= parseInt(prompt("Enter the second number"));
+// var c= parseInt(prompt("Enter the third number"));
+//
+// if (a===""||b===""||c===""){
+// alert("Try Again")
+// }
+//
+// else if(a===b && b===c){
+// alert( "This is an Equilateral triangle")
+// }
+// else if(a===b || b===c){
+// alert("This is an Isosceles triangle")
+// }
+// else if(a+b<c || a+c<b ||b+c<a){
+// alert("NOT a triangle")
+// }
+// else if(a+b>c || a+c<b || b+c>a){
+// alert("This is a scalene triangle")
+// }
+// else {
+// alert("Try Again")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ function triangle(){
  var triangle;
  triangle = document.getElementById("enter a number").value;
  }
@@ -6,6 +53,9 @@ var a= parseInt(prompt("Enter the first number"));
 var b= parseInt(prompt("Enter the second number"));
 var c= parseInt(prompt("Enter the third number"));
 
+var ab= a+b;
+var bc= b+c;
+var ac= a+c;
 if (a===""||b===""||c===""){
 alert("Try Again")
 }
@@ -19,10 +69,14 @@ alert("This is an Isosceles triangle")
 else if(a+b<c || a+c<b ||b+c<a){
 alert("NOT a triangle")
 }
-else if(a+b>c || a+c>b || b+c>a){
+else if(a+b>c || a+c<b || b+c>a){
 alert("This is a scalene triangle")
 }
 else {
-alert("Try Again! Empty field")
+alert("Try Again")
 
 }
+var enters = ["Refresh","the page!"];
+enters.forEach(function(enter){
+alert(word)
+})
