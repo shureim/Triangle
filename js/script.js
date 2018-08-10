85e774c18bfbb19d942a53a9bb53ecc997edbc66
tarick ali <tarickaliabdi@gmail.com>
2018-08-10 15:29:11 +0300

    corrected some errors in the script.file

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
 
-function triangle(){
-var triangle;
-triangle = document.getElementById("enter a number").value;
-}
+var triangle=function(){
+
 var a= parseInt(prompt("Enter the first number"));
 var b= parseInt(prompt("Enter the second number"));
 var c= parseInt(prompt("Enter the third number"));
 
-var ab= a+b;
-var bc= b+c;
-var ac= a+c;
 if (a===""||b===""||c===""){
-alert("Try Again")
+alert("Try Again");
 }
 
 else if(a===b && b===c){
-alert( "This is an Equilateral triangle")
+alert( "This is an Equilateral triangle");
 }
 else if(a===b || b===c){
-alert("This is an Isosceles triangle")
+alert("This is an Isosceles triangle");
 }
 else if(a+b<c || a+c<b ||b+c<a){
-alert("NOT a triangle")
+alert("NOT a triangle");
 }
 else if(a+b>c || a+c<b || b+c>a){
-alert("This is a scalene triangle")
+alert("This is a scalene triangle");
 }
 else {
-alert("Try Again")
+alert("Try Again");
 
 }
+}
+triangle();
+
 var words = ["Refresh","the page!"];
 words.forEach(function(word){
-alert(word)
-}
+alert(word);
+});
