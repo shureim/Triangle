9634ec84a44077ce4b73afe550845e2da4b0a561
tarick ali <tarickaliabdi@gmail.com>
2018-08-10 10:12:17 +0300

    added the alert in script.js file

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@
 var a= prompt("Enter the first number");
 var b= prompt("Enter the second number");
 var c= prompt("Enter the third number");
+if(a===b && b===c){
+alert( "This is an Equilateral triangle")
+}
+else if(a===b || b===c){
+alert("This is an Isosceles triangle")
+}
+else if(a!==b && b!==c){
+alert("This is a scalene triangle ")
+}
+else if((a+b)<=c (a+c)<=b(b+c)<=a){
+alert("NOT a triangle")
+} 
